147adc83684ce1f83a19349f85a541fdd7dcab7b
shiftz300 <kasetry300@outlook.com>
2023-07-11 09:06:26 +0800

    none

--- src/views/Banned.vue
+++ src/views/Banned.vue
@@ -2,8 +2,8 @@
   <div class="account mdui-container">
       <h1 class="qwingin-left-line qwingin-font-teal">小黑屋</h1>
       <div class="mdui-panel" mdui-panel>
-        <div class="mdui-chip" v-for="name in users" :key="name">
-          <span class="mdui-chip-title">{{ name }}</span>
+        <div class="mdui-chip" v-for="bannedUsers in users" :key="bannedUsers">
+          <span class="mdui-chip-title">{{ bannedUsers }}</span>
         </div>
       </div>
   </div>
