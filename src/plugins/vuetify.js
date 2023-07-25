abb843370aacf0b5a43b3a61de23c9c032582557
XiaMoHuaHuo_CN <xmcn@1l1.icu>
2023-07-25 17:08:26 +0800

    Remove some thing

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -21,11 +21,6 @@ if (themeMedia.matches) {
   defTheme = "dark";
 }
 
-作者：ybc4652
-链接：https://juejin.cn/post/6966794966165094414
-来源：稀土掘金
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   blueprint: md3,
