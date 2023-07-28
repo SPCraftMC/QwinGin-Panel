9d20c704c7ed29d120cd0a568321087154533b3c
shiftz <130174875+shiftz300@users.noreply.github.com>
2023-07-28 13:01:26 +0800

    Update LoginDialog.vue

--- src/components/LoginDialog.vue
+++ src/components/LoginDialog.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card
-    width="60vw"
+    width="60vmin"
   >
     <div class="d-flex flex-row">
       <v-tabs
@@ -21,11 +21,11 @@
       </v-tabs>
       <v-window v-model="tab">
         <v-window-item value="login">
-          <v-card width="60vw">
-            <v-text-field v-model="loginData.name" label="用户名" variant="solo-filled" class="mx-6 mt-10"></v-text-field>
-            <v-text-field v-model="loginData.password" label="密码" variant="solo-filled" class="mx-6"></v-text-field>
-            <v-card-item class="text-right" width="24vw">
-              <v-btn @click=clogin.login(loginData) size="large" rounded="s-xl" height="4vh" variant="tonal" class="mr-2">
+          <v-card width="60vmin">
+            <v-text-field v-model="loginData.name" label="用户名" variant="solo-filled" class="mx-4 mt-10"></v-text-field>
+            <v-text-field v-model="loginData.password" label="密码" variant="solo-filled" class="mx-4"></v-text-field>
+            <v-card-item class="text-right" width="24vmin">
+              <v-btn @click=clogin.login(loginData) size="large" rounded="s-xl" height="4vmin" variant="tonal" class="mr-2">
                 登录
               </v-btn>
             </v-card-item>
