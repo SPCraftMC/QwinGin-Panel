e56b1e8f1206f39997ae21bdc367d6d06d3c3a96
夏沫花火zzz <xmcn@1l1.icu>
2023-08-26 02:13:26 +0800

    ?

--- src/layout/header.vue
+++ src/layout/header.vue
@@ -3,12 +3,12 @@
 
     <div class="qwingin-float-right">
         <el-avatar size="64" :src=store.getters.userInfo.avatar.gravatar />
-    <el-button type="primary" :icon="Gird" />
+        <!--<el-button type="primary" :icon="Gird" />-->
     </div>
 </template>
 
 <script setup>
 import store from '@/src/script/vuex/store'
 
-import { Gird } from '@element-plus/icons-vue'
+//import { Gird } from '@element-plus/icons-vue'
 </script>
